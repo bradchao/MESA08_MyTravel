b78eede26a1ee37f2059d5e8e70a4138c17e30a0
Brad <brad@brad.tw>
2017-09-28 09:45:58 +0800

    Initial Commit

---
+++ MyTravel/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  MyTravel
+//
+//  Created by user22 on 2017/9/28.
+//  Copyright © 2017年 Brad Big Company. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+#import <sqlite3.h>
+
+#endif /* Header_h */
---
+++ MyTravel/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyTravel
+//
+//  Created by user22 on 2017/9/28.
+//  Copyright © 2017年 Brad Big Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
